9092cf7f199e2543927840b3c8d8ea95eefca871
escowin <edwin@escowinart.com>
2022-08-11 18:11:32 -0500

    js | getWeather() succesfully fetches NASA Insight API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
 // mars weather | insight weather api
+// dom | elements
+
+
+// api
+const apiKey = 'DEMO_KEY';
+const apiURL = `https://api.nasa.gov/insight_weather/?api_key=${apiKey}&feedtype=json&ver=1.0`
+
+
+var getWeather = function() {
+    fetch(apiURL).then(res => res.json())
+    .then(data => {
+        console.log(data);
+    })
+};
+
+getWeather();
