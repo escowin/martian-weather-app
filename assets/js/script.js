4dcd74c384a2364d5b43bec8d3b486b57c7cda9b
escowin <edwin@escowinart.com>
2022-08-11 18:23:41 -0500

    js:13 | destructured relevant data

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,13 @@
 const apiKey = 'DEMO_KEY';
 const apiURL = `https://api.nasa.gov/insight_weather/?api_key=${apiKey}&feedtype=json&ver=1.0`
 
-
+// logic | get data
 var getWeather = function() {
     fetch(apiURL).then(res => res.json())
     .then(data => {
+        const {
+            sol_keys, validity_checks, ...solData
+        } = data
         console.log(data);
     })
 };
