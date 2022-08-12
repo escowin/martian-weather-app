ea3eda19bca0cd2d5b95c7fe27bbb74585596c9c
escowin <edwin@escowinart.com>
2022-08-11 22:43:50 -0500

    js:9 | note

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,9 @@
 
 // api
 const apiKey = 'DEMO_KEY';
-const apiURL = `https://api.nasa.gov/insight_weather/?api_key=${apiKey}&feedtype=json&ver=1.0`
+// const apiURL = `https://api.nasa.gov/insight_weather/?api_key=${apiKey}&feedtype=json&ver=1.0`
+
+// pause: InSight API down
 
 // logic | get data
 var getWeather = function() {
